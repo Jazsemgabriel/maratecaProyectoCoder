165902cf20eb9afe453f0ad19bc3c347920bc7d3
gabriel <gabrieljrt.94@gmail.com>
2021-10-19 00:15:38 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,136 @@
+//Definición de variables//
+const baseProductos = [
+    {
+      nombre: "TV Samsung UHD",
+      precio: 1800,
+      imagen: "/img/samsung.tv.png"
+    },
+  
+    {
+      nombre: "TV LG UHD",
+      precio: 1600,
+      imagen: "/img/lg.tv.png"
+    },
+  
+    {
+      nombre: "TV Sony UHD",
+      precio: 2500,
+      imagen: "/img/sony.tv.png"
+    },
+  
+    {
+      nombre: "Roku TV",
+      precio: 150,
+      imagen: "/img/rokup.jpg"
+    },
+  
+    {
+      nombre: "Chromecast TV",
+      precio: 200,
+      imagen: "/img/chromecast.jpg"
+    },
+  
+    {
+      nombre: "Fire TV",
+      precio: 250,
+      imagen: "/img/firetv.png"
+    },
+  ];
+
+class Carrito {
+  constructor(object) {
+    if (object) {
+        Object.assign(this,object)
+    } else {
+      this.productos = [0, 0, 0, 0, 0, 0];
+    }
+  }
+
+  calcularTotalPagar() {
+    var total = 0
+    for (var i = 0; i < this.productos.length; i++) {
+        total = total + this.productos[i] * baseProductos[i].precio
+    }
+    return total ;
+  }
+
+  estaVacio() {
+    for (var i = 0; i < this.productos.length; i++) {
+      if (this.productos[i] > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  agregarProducto(codigo) {
+    this.productos[codigo - 1] = this.productos[codigo - 1] + 1;
+  }
+
+  quitarProducto(codigo) {
+    this.productos[codigo - 1] = this.productos[codigo - 1] - 1;
+  }
+}
+
+if (localStorage.getItem("carrito") == null) {
+  localStorage.setItem("carrito", JSON.stringify(new Carrito()));
+} else {
+}
+
+actualizarCarritoDOM()
+
+var costoTotal = 0;
+
+$('.add_cart_btn').click(function(){
+  var codigoProducto = parseInt(this.getAttribute("data-id"));
+  var carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+  var carritoTemporal = new Carrito(carritoStorage);
+  carritoTemporal.agregarProducto(codigoProducto);
+  localStorage.setItem("carrito", JSON.stringify(carritoTemporal))
+
+  // Actualizar el carrito en el DOM
+
+  actualizarCarritoDOM()
+})
+
+  // Remover productos del carrito
+
+$('body').on('click',".remove_cart_btn",function(){
+  var codigoProducto = parseInt(this.getAttribute("data-id"));
+  var carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+  var carritoTemporal = new Carrito(carritoStorage);
+  carritoTemporal.quitarProducto(codigoProducto);
+  localStorage.setItem("carrito", JSON.stringify(carritoTemporal))
+
+  actualizarCarritoDOM()
+})
+
+function actualizarCarritoDOM() {
+    var carritoStorage = JSON.parse(localStorage.getItem("carrito"))
+    var carrito = new Carrito(carritoStorage)
+    if (carrito.estaVacio()){
+      $('#carrito-resumen').html(`<p> Agrega un producto </p>`)
+    } else {
+        var htmlProductos = ``
+        for (var i = 0;  i < carrito.productos.length; i++) {
+            if (carrito.productos[i] > 0) {
+                var html = `<div class="carrito-resumen-item d-flex align-items-center justify-content-between my-2">
+                <img src="${baseProductos[i].imagen}" alt="">
+                <div>
+                    <div>${baseProductos[i].nombre}</div>
+                    <div> Cantidad: <span> ${carrito.productos[i]} </span> </div>
+                </div>
+                <button class="btn btn-danger remove_cart_btn" data-id="${i+1}" type="button">-</button>
+            </div>`
+                htmlProductos = htmlProductos + html
+            }
+
+        }
+        
+        $('#carrito-resumen').html(htmlProductos)
+    }
+
+    $('#precioTotal').html('S/' + carrito.calcularTotalPagar())
+
+}
+
